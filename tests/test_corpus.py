7d8530c38e684ce56f784fcc0f502f0d3ce042e7
Gunther Cox <gunthercx@gmail.com>
2017-03-15 10:30:33 -0400

    Fix broken tests

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -1,6 +1,9 @@
+"""
+Tests for the chatterbot corpus package.
+"""
 import os
 from unittest import TestCase
-from corpus.corpus import Corpus
+from chatterbot_corpus.corpus import Corpus
 
 
 class CorpusUtilsTestCase(TestCase):
@@ -12,34 +15,30 @@ class CorpusUtilsTestCase(TestCase):
         """
         Test that a dotted path is properly converted to a file address.
         """
-        path = self.corpus.get_file_path('corpus.data.english')
+        path = self.corpus.get_file_path('chatterbot.corpus.english')
         self.assertIn(
-            os.path.join('corpus', 'data', 'english'),
+            os.path.join('chatterbot_corpus', 'data', 'english'),
             path
         )
 
     def test_read_english_corpus(self):
         corpus_path = os.path.join(
-            'corpus', 'data', 'english', 'conversations.corpus.json'
+            self.corpus.data_directory,
+            'english', 'conversations.corpus.json'
         )
         data = self.corpus.read_corpus(corpus_path)
         self.assertIn('conversations', data)
 
     def test_list_english_corpus_files(self):
-        data_files = self.corpus.list_corpus_files('corpus.data.english')
+        data_files = self.corpus.list_corpus_files('chatterbot.corpus.english')
 
         self.assertIn('.json', data_files[0])
 
     def test_load_corpus(self):
-        corpus = self.corpus.load_corpus('corpus.data.english.greetings')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.english.greetings')
 
         self.assertEqual(len(corpus), 1)
         self.assertIn(['Hi', 'Hello'], corpus[0])
-
-    def test_load_corpus_english(self):
-        corpus = self.corpus.load_corpus("corpus.data.english")
-        self.assertEqual(len(corpus), 19)
-        self.assertIn(corpus[1], corpus)
 
 
 class CorpusLoadingTestCase(TestCase):
@@ -48,66 +47,66 @@ class CorpusLoadingTestCase(TestCase):
         self.corpus = Corpus()
 
     def test_load_corpus_chinese(self):
-        corpus = self.corpus.load_corpus('corpus.data.chinese')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.chinese')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_traditional_chinese(self):
-        corpus = self.corpus.load_corpus('corpus.data.tchinese')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.tchinese')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_english(self):
-        corpus = self.corpus.load_corpus('corpus.data.english')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.english')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_french(self):
-        corpus = self.corpus.load_corpus('corpus.data.french')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.french')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_german(self):
-        corpus = self.corpus.load_corpus('corpus.data.german')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.german')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_hindi(self):
-        corpus = self.corpus.load_corpus('corpus.data.hindi')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.hindi')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_indonesia(self):
-        corpus = self.corpus.load_corpus('corpus.data.indonesia')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.indonesia')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_italian(self):
-        corpus = self.corpus.load_corpus('corpus.data.italian')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.italian')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_marathi(self):
-        corpus = self.corpus.load_corpus('corpus.data.marathi')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.marathi')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_portuguese(self):
-        corpus = self.corpus.load_corpus('corpus.data.portuguese')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.portuguese')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_russian(self):
-        corpus = self.corpus.load_corpus('corpus.data.russian')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.russian')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_spanish(self):
-        corpus = self.corpus.load_corpus('corpus.data.spanish')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.spanish')
 
         self.assertTrue(len(corpus))
 
     def test_load_corpus_telugu(self):
-        corpus = self.corpus.load_corpus('corpus.data.telugu')
+        corpus = self.corpus.load_corpus('chatterbot.corpus.telugu')
 
         self.assertTrue(len(corpus))
