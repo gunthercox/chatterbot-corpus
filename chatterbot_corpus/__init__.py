27d75dfe3063169aaf9f4abfab9a1af98e403ae8
Gunther Cox <gunthercx@gmail.com>
2018-10-06 07:19:46 -0400

    Update version to 1.2.0

--- chatterbot_corpus/__init__.py
+++ chatterbot_corpus/__init__.py
@@ -2,7 +2,7 @@
 A machine readable multilingual dialog corpus.
 """
 
-__version__ = '1.1.4'
+__version__ = '1.2.0'
 __author__ = 'Gunther Cox'
 __email__ = 'gunthercx@gmail.com'
 __url__ = 'https://github.com/gunthercox/chatterbot-corpus'
