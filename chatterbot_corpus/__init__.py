572d17ff6d1e92f270f9dfd6231d4cd7b1f567e7
Gunther Cox <gunthercx@gmail.com>
2025-02-10 06:51:07 -0500

    Increment version number

--- chatterbot_corpus/__init__.py
+++ chatterbot_corpus/__init__.py
@@ -2,7 +2,7 @@
 A machine readable multilingual dialog corpus.
 """
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 __author__ = 'Gunther Cox'
 __email__ = 'gunthercx@gmail.com'
 __url__ = 'https://github.com/gunthercox/chatterbot-corpus'
